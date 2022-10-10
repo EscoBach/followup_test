dca4f102cdecae5ca3c89db4836c8446073a7b5e
Escobar-Bach Mikael <m.escobar@gpu.math.ua>
2022-10-11 00:20:17 +0200

    git_init

---
+++ workflow.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+git fetch
+
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+
+if test "$BRANCH" != "main"
+then
+
+	#create log file
+	#log=logs/log_$(date +"%d-%m-%Y").txt
+	#printf "Log File - " > $log
+	#date >> $log
+
+	R CMD BATCH main.r 
+
+	git add -A 
+
+	git commit -m 'auto_git'
+
+	git push
+fi
+
+
